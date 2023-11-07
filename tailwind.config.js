ee6c238c1b64f6205939c8d9f317f6db90067f82
Fabio Perez <65570296+fanpero87@users.noreply.github.com>
2023-11-07 17:00:15 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./*.html', './src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			screens: {
+				mobile: '375px',
+				desktop: '1440px',
+			},
+			colors: {
+				'qr-white': 'hsl(0, 0%, 100%)',
+				'qr-light-gray': 'hsl(212, 45%, 89%)',
+				'qr-grayish-blue': 'hsl(220, 15%, 55%)',
+				'qr-dark-blue': 'hsl(218, 44%, 22%)',
+			},
+			fontSize: {
+				'qr-font-size': '15px',
+			},
+			fontFamily: {
+				outfit: 'Outfit, sans-serif',
+			},
+		},
+	},
+	plugins: [],
+};
